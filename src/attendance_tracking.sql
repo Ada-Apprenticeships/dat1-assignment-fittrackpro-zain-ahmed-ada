3ec77267c8b04235ed9fda171dfe4c7602454d4b
Zain Ahmed <zain.ahmed@ada.ac.uk>
2025-02-17 00:47:22 +0000

    Complete attendance tracking queries

--- src/attendance_tracking.sql
+++ src/attendance_tracking.sql
@@ -2,18 +2,41 @@
 .open fittrackpro.db
 .mode column
 
--- Enable foreign key support
+PRAGMA foreign_keys = ON;
 
 -- Attendance Tracking Queries
 
 -- 1. Record a member's gym visit
--- TODO: Write a query to record a member's gym visit
+INSERT INTO attendance(attendance_id, check_in_time, member_id, location_id)
+VALUES (11, CURRENT_TIMESTAMP, 7, 1);
 
 -- 2. Retrieve a member's attendance history
--- TODO: Write a query to retrieve a member's attendance history
+SELECT DATE(check_in_time) AS visit_date, check_in_time, check_out_time
+FROM attendance
+WHERE member_id = 5;
 
 -- 3. Find the busiest day of the week based on gym visits
--- TODO: Write a query to find the busiest day of the week based on gym visits
+SELECT 
+    CASE strftime('%w', check_in_time)
+        WHEN '0' THEN 'Sunday'
+        WHEN '1' THEN 'Monday'
+        WHEN '2' THEN 'Tuesday'
+        WHEN '3' THEN 'Wednesday'
+        WHEN '4' THEN 'Thursday'
+        WHEN '5' THEN 'Friday'
+        ELSE 'Saturday'
+    END AS day_of_week,
+    COUNT(*) AS visit_count
+FROM 
+    attendance
+GROUP BY
+    day_of_week 
+ORDER BY
+    visit_count DESC
+LIMIT 1;
 
 -- 4. Calculate the average daily attendance for each location
--- TODO: Write a query to calculate the average daily attendance for each location
+SELECT l.name AS location_name, CAST(COUNT (a.location_id) AS REAL) / COUNT(DISTINCT DATE(a.check_in_time)) AS avg_daily_attendance
+FROM attendance a
+INNER JOIN locations l on a.location_id = l.location_id
+GROUP BY a.location_id;
